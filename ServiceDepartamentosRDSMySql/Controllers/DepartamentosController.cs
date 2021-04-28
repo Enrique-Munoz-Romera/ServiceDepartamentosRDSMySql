f2c52f402f844b8e2fe0adec2a244fd9eeec7c6d
Enrique Muñoz <enriquemunozromera@outlook.es>
2021-04-28 11:19:35 +0200

    Add project files.

---
+++ ServiceDepartamentosRDSMySql/Controllers/DepartamentosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceDepartamentosRDSMySql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceDepartamentosRDSMySql.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        Repositories.RepositoryDepartamentos repository;
+
+        public DepartamentosController(Repositories.RepositoryDepartamentos repository) { this.repository = repository; }
+
+        [HttpGet]
+        public List<Departamento> GetDepartamentos() { return this.repository.GetDepartamentos(); }
+
+        [HttpGet("{id}")]
+        public Departamento GetDepartamento(int id) { return this.repository.FindDepartamento(id); }
+
+
+    }
+}
